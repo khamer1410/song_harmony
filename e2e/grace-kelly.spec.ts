1bbf8e7c7877607aba6dc3dca7ad2cd419222d40
Krzysztof Hamerszmit <krzysztof.hamerszmit@syzygy.pl>
2023-10-11 22:39:14 +0200

    play / pause functionality

--- e2e/grace-kelly.spec.ts
+++ e2e/grace-kelly.spec.ts
@@ -7,3 +7,14 @@ test('when I enter the song page then I see the artist and song name', async ({p
   await page.goto(GRACE_URL);
   await expect(page.getByRole('heading', {name: ARTIST_NAME})).toBeVisible()
 })
+
+test('when I click play button then all voicings are being played', async ({page})=> {
+  await page.goto(GRACE_URL);
+  await page.click('button')
+
+  const isAudioPlaying = await page.getByTestId('audio')
+  console.log(isAudioPlaying);
+
+// TODO: how assert on audio being played?
+  await expect(isAudioPlaying).toBeDefined()
+})
--- src/routes/grace-kelly/+page.svelte
+++ src/routes/grace-kelly/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>Grace Kelly</title>
+  <title>{songData.song_title}</title>
 </svelte:head>
 
 <script lang='ts'>
@@ -9,24 +9,52 @@
 
   let songData = {} as SongHarmony
   let paused = true
+  let muted = false
 
   onMount(async ()=> {
     const data = await getSongHarmony()
     console.log(data);
     songData = data
   })
+
+  function playFromStart() {
+    const audios = document.querySelectorAll('audio')
+    audios.forEach(audio => {
+      audio.currentTime = 0
+    })
+    paused = false;
+  }
+
+  function pauseAll() {
+    const audios = document.querySelectorAll('audio')
+    audios.forEach(audio => {
+      audio.currentTime = 0
+    })
+    paused = true
+  }
 </script>
 
 <!-- TODO: loading state -->
 <h1>{songData.artist} - {songData.song_title}</h1>
+<button
+class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+on:click={playFromStart}>PLAY</button>
 
+<button
+class="uppercase bg-yellow-400 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+on:click={pauseAll}>REWIND</button>
+
+<button
+class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+on:click={() => muted = !muted}>MUTE</button>
+
+<hr class="my-4">
 {#if songData.voicings?.length}
    {#each songData.voicings as voice}
-   <!-- TODO: make a component -->
-     <section class='flex'>
+   <!-- TODO: make a component, make mutable -->
+     <section class='flex gap-2'>
        <h2>{voice.name}</h2>
-       <button on:click={() => paused = !paused}>PLAY</button>
-       <audio src={voice.audio} bind:paused={paused}/>
+       <audio data-testid='audio' src={voice.audio} bind:paused={paused} bind:muted={muted}/>
      </section>
    {/each}
 {/if}
