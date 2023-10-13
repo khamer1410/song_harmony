78100ad2993b97566b09cb0ddd3f885b6fa1c0da
Krzysztof Hamerszmit <krzysztof.hamerszmit@syzygy.pl>
2023-10-13 15:51:10 +0200

    Add simple routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+  <navigation class='py-6 block'>
+    <h2>Pages: </h2>
+  	<a href="/grace-kelly" class="underline to-blue-700 hover:to-blue-400">Grace Kelly page</a>
+  </navigation>
 
-<h1 class="underline first-letter:bg-red-600">
-  Hello world!
-</h1>
+  <h1 class="first-letter:bg-red-600">Hello world!</h1>
+</main>
