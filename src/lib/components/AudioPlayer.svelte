0b69ff964a9ab7ce701d83c7bb680956c9ff4718
Krzysztof Hamerszmit <krzysztof.hamerszmit@syzygy.pl>
2023-11-25 22:57:43 +0100

    🎹 add mutable AudioPlayer component, add basic playing global controls

---
+++ src/lib/components/AudioPlayer.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+export let src: string;
+export let muted: boolean;
+export let name: string;
+export let paused: boolean;
+
+</script>
+
+<section class="flex gap-2">
+	<audio {src} {muted} controls bind:paused={paused}/>
+	<h2>{name}</h2>
+	<label class="inline-flex items-center cursor-pointer ">
+		<input
+			type="checkbox"
+			class="form-checkbox h-5 w-5 text-indigo-600 transition duration-150 ease-in-out cursor-pointer"
+			bind:checked={muted}
+		/>
+		<span class="ml-2 text-sm leading-5 text-gray-700">{muted ? 'Unmute' : 'Mute'}</span>
+	</label>
+</section>
--- src/routes/grace-kelly/+page.svelte
+++ src/routes/grace-kelly/+page.svelte
@@ -1,55 +1,83 @@
-<svelte:head>
-  <title>Grace Kelly</title>
-</svelte:head>
+<script lang="ts">
+	import { getSongHarmony } from '$lib';
+	import AudioPlayer from '@/lib/components/AudioPlayer.svelte';
 
-<script lang='ts'>
-	import { getSongHarmony } from "$lib";
+	const songData = getSongHarmony();
 
-  let songData = getSongHarmony()
-  let paused = true
-  let muted = false
+	let allMuted = false;
+	let allPlaying = false;
+	let looping = false;
 
-  function playFromStart() {
-    const audios = document.querySelectorAll('audio')
-    audios.forEach(audio => {
-      audio.currentTime = 0
-    })
-    paused = false;
-  }
+	function playFromStart() {
+		const audios = document.querySelectorAll('audio');
+		audios.forEach((audio) => {
+			audio.currentTime = 0;
+			audio.play();
+		});
+		allPlaying = true;
+	}
 
-  function pauseAll() {
-    const audios = document.querySelectorAll('audio')
-    audios.forEach(audio => {
-      audio.currentTime = 0
-    })
-    paused = true
-  }
+	function togglePlay() {
+		if (!allPlaying) {
+			const audios = document.querySelectorAll('audio');
+			audios.forEach((audio) => {
+				audio.play();
+			});
+		} else {
+			const audios = document.querySelectorAll('audio');
+			audios.forEach((audio) => {
+				audio.pause();
+			});
+		}
+		allPlaying = !allPlaying;
+	}
+
+	function toggleLoop() {
+		looping = !looping;
+		const audios = document.querySelectorAll('audio');
+		audios.forEach((audio) => {
+			audio.loop = looping;
+		});
+	}
 </script>
 
-{#await songData}
-<h2>LOADING ...</h2>
-{:then songData}
-<h1>{songData.artist} - {songData.song_title}</h1>
-<button
-class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-on:click={playFromStart}>PLAY</button>
+<svelte:head>
+	<title>Grace Kelly</title>
+</svelte:head>
 
-<button
-class="uppercase bg-yellow-400 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-on:click={pauseAll}>REWIND</button>
+<main class="container mx-auto px-4">
+	{#await songData}
+		<h2>LOADING ...</h2>
+	{:then songData}
+		<h1>{songData.artist} - {songData.song_title}</h1>
+		<button
+			class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+			on:click={togglePlay}>{allPlaying ? 'PAUSE' : 'PLAY'}</button
+		>
 
-<button
-class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-on:click={() => muted = !muted}>MUTE</button>
+		<button
+			class="uppercase bg-yellow-400 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+			on:click={playFromStart}>REWIND</button
+		>
 
-<hr class="my-4">
-{#if songData.voicings?.length}
-   {#each songData.voicings as voice}
-   <!-- TODO: make a component, make mutable -->
-     <section class='flex gap-2'>
-       <h2>{voice.name}</h2>
-       <audio data-testid='audio' src={voice.audio} bind:paused={paused} bind:muted={muted}/>
-     </section>
-   {/each}
-{/if}
-{/await}
+		<button
+			class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+			on:click={() => (allMuted = !allMuted)}>MUTE</button
+		>
+
+		<button
+			disabled
+			class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+			on:click={toggleLoop}>{looping ? 'LOOPING' : 'LOOP'}</button
+		>
+
+		<hr class="my-4" />
+		<section class="flex flex-col gap-2">
+			{#if songData.voicings.length}
+				{#each songData.voicings as voice}
+					<AudioPlayer src={voice.audio} muted={allMuted} name={voice.name} paused={!allPlaying} />
+				{/each}
+			{/if}
+		</section>
+	{/await}
+</main>
